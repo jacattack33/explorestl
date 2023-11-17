47f1d7899883112d6e0e4b7337e7e99397d5c5b8
Jacque <jacquemueller2468@gmail.com>
2023-11-16 21:07:22 -0600

    fixed js related to completed button, rename completed buttons

--- explorestl/wwwroot/js/site.js
+++ explorestl/wwwroot/js/site.js
@@ -1,22 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-function changeRemoveButton(id) {
-    document.getElementById("filter_btn_" + id).value = "Sighting Removed!";
-    document.getElementById("filter_btn_" + id).style.backgroundColor = 'red';
-    document.getElementById("filter_btn_" + id).style.color = 'white';
-}
-
-function changeAddFavoritesButton(id) {
-    console.log(id)
+﻿function changeAddFavoritesButton(id) {
     document.getElementById("filter_btn_" + id).value = "Added to List!";
     document.getElementById("filter_btn_" + id).style.backgroundColor = 'dodgerblue';
     document.getElementById("filter_btn_" + id).style.color = 'white';
 }
 
 function changeCompletedButton(id) {
-    document.getElemntById("filter_btn_" + id).value = "Completed!";
+    debugger
+    document.getElementById("filter_btn2_" + id).value = "Completed!";
+    document.getElementById("filter_btn2_" + id).style.backgroundColor = 'red';
+    document.getElementById("filter_btn2_" + id).style.color = 'white';
+}
+
+function changeRemoveButton(id) {
+    document.getElementById("filter_btn_" + id).value = "Sighting Removed!";
+    document.getElementById("filter_btn_" + id).style.backgroundColor = 'red';
+    document.getElementById("filter_btn_" + id).style.color = 'white';
+}
+
+function changeRemoveCompletedButton(id) {
+    document.getElementById("filter_btn_" + id).value = "Activity Removed from Completed!";
     document.getElementById("filter_btn_" + id).style.backgroundColor = 'red';
     document.getElementById("filter_btn_" + id).style.color = 'white';
 }
@@ -25,14 +27,3 @@ function changeCompletedButton(id) {
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-/*for (i = 0; i <= coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}*/
